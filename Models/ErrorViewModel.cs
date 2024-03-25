e7193d345e2fa43db57372384b4729f743eea0a2
Jasper Jan Tan <jasperjantan1996@gmail.com>
2024-03-25 14:13:05 -0400

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Q4DevOps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
